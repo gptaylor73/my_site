d435c6010f91178410b3ed426d8d240dacd836dc
greg <gp.taylor73@gmail.com>
2023-09-15 09:07:43 +0400

    bla

--- book_store/models.py
+++ book_store/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MaxValueValidator(5),
+                                             MinValueValidator(1)])
+    author = models.CharField(max_length=100, null=True)
+    is_bestselling = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.title} ({self.rating})'
 
